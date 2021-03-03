f423dce22b26e9679acc7008e5131893f36fb346
Alex Necula <florin-alexandru.necula@my.fmi.unibuc.ro>
2021-03-03 22:10:25 +0200

    Added functionalities to the Balloon Pop game

--- src/components/balloonPop/balloonPop.js
+++ src/components/balloonPop/balloonPop.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useRef, useState, useLayoutEffect } from "react";
 import { Link } from 'react-router-dom';
 import bored from '../../bored.svg';
+import balloon from '../../balloon.svg';
+import reset from '../../settings.svg';
 
 const BalloonPop = () => {
     document.title = "Balloon Pop";
@@ -10,6 +12,8 @@ const BalloonPop = () => {
     const GameArea = () => {
         const targetRef = useRef();
         const [dimensions, setDimensions] = useState({ width:0, height: 0 });
+        const [clicks, setClicks] = useState(0);
+        const [correctClicks, setCorrectClicks] = useState(0);
         let dotPosition = {x: 0, y: 0};
         let timeout = false;
         let delay = 250;
@@ -17,7 +21,6 @@ const BalloonPop = () => {
         const [styles, setStyles] = useState({
             height: '75px',
             width: '75px',
-            backgroundColor: '#09d3ac',
             borderRadius: '50%',
             position: 'absolute',
         });
@@ -35,6 +38,19 @@ const BalloonPop = () => {
             return Math.random() * (max - min) + min;
         }
 
+        function changePosition() {
+            setCorrectClicks(correctClicks + 1);
+            dotPosition.x = getRandomArbitrary(0, dimensions.width - 75);
+            dotPosition.y = getRandomArbitrary(165, dimensions.height);
+            setStyles({
+                height: '75px',
+                width: '75px',
+                position: 'absolute',
+                top: dotPosition.y,
+                left: dotPosition.x,
+            });
+        }
+
         function getSize() {
             if (targetRef.current) {
                 setDimensions({
@@ -44,30 +60,39 @@ const BalloonPop = () => {
             }
         }
 
-        function changePosition() {
-            dotPosition.x = getRandomArbitrary(0, dimensions.width - 75);
-            dotPosition.y = getRandomArbitrary(100, dimensions.height);
+        window.addEventListener('resize', function() {
+            clearTimeout(timeout);
+            timeout = setTimeout(getSize, delay);
+        });
+
+        function countClicks() {
+            setClicks(clicks + 1);
+        }
+
+        function resetGame() {
+            dotPosition.x = 0;
+            dotPosition.y = 165;
+
+            setClicks(0);
+            setCorrectClicks(0);
+
             setStyles({
                 height: '75px',
                 width: '75px',
-                backgroundColor: '#09d3ac',
-                borderRadius: '50%',
                 position: 'absolute',
                 top: dotPosition.y,
                 left: dotPosition.x,
             });
         }
 
-        window.addEventListener('resize', function() {
-            clearTimeout(timeout);
-            timeout = setTimeout(getSize, delay);
-        });
-
         return (
-            <div className = "GameArea" ref={targetRef}>
-                <p>{dimensions.height}</p>
-                <p>{dimensions.width}</p>
-                <span style={styles} onClick={changePosition}></span>
+            <div>
+                <img className="Reset" src={reset} alt="reset" onClick={resetGame}/>
+                <div className = "GameArea" ref={targetRef} onClick={countClicks}>
+                    <p>Total clicks: {clicks}</p>
+                    <p>Correct clicks: {correctClicks}</p>
+                    <img src={balloon} style={styles} alt="balloon" onClick={changePosition} />
+                </div>
             </div>
         );
     };
